063ca31d0b4e6a2cece0182cd107ad9bebb35633
Alex Chapin <achapin@gmail.com>
2010-08-04 11:09:10 -0400

    updated 2010 index so that container css includes sidebar

--- index.php
+++ index.php
@@ -26,7 +26,9 @@ get_header(); ?>
 			 get_template_part( 'loop', 'index' );
 			?>
 			</div><!-- #content -->
-		</div><!-- #container -->
+
 
 <?php get_sidebar(); ?>
+		</div><!-- #container -->
 <?php get_footer(); ?>
+
