e107e1a91754ee68f8f73ffe8ba54283f1309214
Chapin <achapin@achapin-7660.local>
2010-08-05 20:37:50 -0400

    added background attachment option to the theme config

--- config-sample.php
+++ config-sample.php
@@ -154,6 +154,7 @@ $variation_config['model'][68]  = 'header-text-shadow-offset';
 $variation_config['model'][69]  = 'header-text-shadow-blur';
 $variation_config['model'][70]  = 'header-text-shadow-color';
 $variation_config['model'][71]  = 'colophon-border-type';
+$variation_config['model'][72]  = 'background_attachment';
 
 /******************************************************************************
  * Disabled Variations 
