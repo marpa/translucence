d3cf738044ee7181dd0c19cd21695d229b1102f3
Chapin <achapin@achapin-7660.local>
2010-08-14 19:59:36 -0400

    updated twentyten_posted_on() function to not display "Posted on" text

--- functions.php
+++ functions.php
@@ -561,7 +561,7 @@ if ( ! function_exists( 'twentyten_posted_on' ) ) :
  * @since Twenty Ten 1.0
  */
 function twentyten_posted_on() {
-	printf( __( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s', 'twentyten' ),
+	printf( __( '<span class="%1$s"></span> %2$s <span class="meta-sep">by</span> %3$s', 'twentyten' ),
 		'meta-prep meta-prep-author',
 		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
 			get_permalink(),
