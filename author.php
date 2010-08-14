b7559df0c27d203773af56fa508be0b766ebb1ca
Chapin <achapin@achapin-7660.local>
2010-08-14 19:08:00 -0400

    added rss feed links to author, category and tag pages and updated css

--- author.php
+++ author.php
@@ -24,21 +24,28 @@ get_header(); ?>
 		the_post();
 ?>
 
-				<h1 class="page-title author"><?php printf( __( 'Author Archives: %s', 'twentyten' ), "<span class='vcard'><a class='url fn n' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a></span>" ); ?></h1>
+<div id="syndication" style="float: right;">
+<a href="<?php print get_author_feed_link( get_the_author_meta( 'ID' ), 'rss2' ); ?>" class="feed"> &#8216;<?php print get_the_author_meta( 'display_name' ) ?>&#8217; Author RSS</a>
+</div>
+<br/>
 
 <?php
 // If a user has filled out their description, show a bio on their entries.
-if ( get_the_author_meta( 'description' ) ) : ?>
+//if ( get_the_author_meta( 'description' ) ) : ?>
 					<div id="entry-author-info">
 						<div id="author-avatar">
 							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
 						</div><!-- #author-avatar -->
-						<div id="author-description">
-							<h2><?php printf( __( 'About %s', 'twentyten' ), get_the_author() ); ?></h2>
-							<?php the_author_meta( 'description' ); ?>
+						<div id="author-description">							
+							<h2><?php print get_the_author(); ?></h2>
+							<div><a href="<?php the_author_meta( 'user_url' ); ?>"><?php the_author_meta( 'user_url' ); ?></a></div>
+							<div><?php the_author_meta( 'description' ); ?></div>
 						</div><!-- #author-description	-->
 					</div><!-- #entry-author-info -->
-<?php endif; ?>
+<?php //endif; ?>
+<div>
+
+<h3>Posts by <?php echo $current_author->display_name; ?></h3>
 
 <?php
 	/* Since we called the_post() above, we need to
