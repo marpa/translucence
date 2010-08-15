9116ce2c8952351b0d0124f159bef23c5b87b677
Alex Chapin <achapin@gmail.com>
2010-08-15 19:37:35 -0400

    added page templates for all variations of sidebar display

---
+++ page-no-sidebar.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template Name: No Sidebars
+ *
+ * A custom page template with no sidebars.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); 
+global $options;
+$content_width = $options['site-width'] - 75;
+?>
+
+		<div id="container">
+		
+			<div id="content" role="main" style="width: <?php print $content_width; ?>px;">
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php if ( is_front_page() ) { ?>
+						<h2 class="entry-title"><?php the_title(); ?></h2>
+					<?php } else { ?>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php } ?>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+
+				<?php comments_template( '', true ); ?>
+
+<?php endwhile; ?>
+
+			</div><!-- #content -->
+
+
+		</div><!-- #container -->
+<?php get_footer(); ?>
