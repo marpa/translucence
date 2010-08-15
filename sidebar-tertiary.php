b52a5be05dba0869ee8e3c8d8aaf5a34977b2213
Alex Chapin <achapin@gmail.com>
2010-08-15 18:43:42 -0400

    declared $options global for the tertiary sidebar so it would hide properly

--- sidebar-tertiary.php
+++ sidebar-tertiary.php
@@ -15,6 +15,7 @@
 	 * some default sidebar stuff just in case.
 	 */
 	// A tertiary sidebar for widgets, just because.
+	global $options;
 	if ( is_active_sidebar( 'tertiary-widget-area' )  && $options['left01-width'] != '0') : ?>
 
 		<div id="tertiary" class="widget-area" role="complementary">
