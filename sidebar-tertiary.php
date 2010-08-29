b6d670e331be25dd13bea248b2b2cade3be4ea7c
Alex Chapin <achapin@gmail.com>
2010-08-29 17:41:22 -0400

    refined tertiary sidebar template and default css for initial condition

--- sidebar-tertiary.php
+++ sidebar-tertiary.php
@@ -16,7 +16,7 @@
 	 */
 	// A tertiary sidebar for widgets, just because.
 	global $options;
-	if ( is_active_sidebar( 'tertiary-widget-area' ) && $options['left01-width'] != '0') : ?>
+	if ( is_active_sidebar( 'tertiary-widget-area' )) : ?>
 
 		<div id="tertiary" class="widget-area" role="complementary">
 			<ul class="xoxo">
