766aca4eeade419cf1f8b4978a290aa609eb857e
Chapin <achapin@achapin-7660.local>
2010-10-25 13:40:38 -0400

    added link to create new page on all page templates

--- page-right01-sidebar.php
+++ page-right01-sidebar.php
@@ -17,9 +17,17 @@ global $options;
 $content_width = $options['site-width'] - $options['right01-width'] - 125;
 ?>
 
-		<div id="container">
-		
-			<div id="content" role="main" style="width: <?php print $content_width; ?>px;">
+<div id="container">
+
+	<div id="content" role="main" style="width: <?php print $content_width; ?>px;">
+
+<?php /* display link to new post if user is at least an author */?>
+<?php if (current_user_can( 'edit_posts' ) && !is_archive() && !is_search()) : ?>
+	<div class='post-link' style="float: right; width: 40%;">
+	<a href="<?php bloginfo('url'); ?>/wp-admin/post-new.php?post_type=page">New Page</a>
+	</div>
+<?php endif; ?>
+
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
