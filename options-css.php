645b15cbc3146cc75f59de40e6159a874cf45dce
Alex Chapin <achapin@gmail.com>
2010-11-21 11:58:44 -0500

    added css for categories and custom menus to options-css

--- options-css.php
+++ options-css.php
@@ -694,23 +694,34 @@ div.menu li {
 /* End Access (top navigation bar)
 -------------------------------------------------------------- */	
 
-#primary .widget_pages li.current_page_item {
+#primary .widget_pages li.current_page_item,  
+#primary .widget_categories li.current-cat,
+#primary .widget_nav_menu li.current-menu-item {
 	background-color: <?php print $options['right01-color-hover-rgb']?>;
 	padding: 3px;
 	border: 1px solid <?php print $options['right01-border-top']?>;
 }
 
-#secondary .widget_pages li.current_page_item {
+#secondary .widget_pages li.current_page_item,
+#secondary .widget_pages li.current-cat,
+#secondary .widget_nav_menu li.current-menu-item {
 	background-color: <?php print $options['right02-color-hover-rgb']?>;
 	padding: 3px;
 	border: 1px solid <?php print $options['right02-border-top']?>;
 }
 
-#tertiary .widget_pages li.current_page_item {
+#tertiary .widget_pages li.current_page_item,
+#tertiary .widget_pages li.current-cat,
+#tertiary .widget_nav_menu li.current-menu-item {
 	background-color: <?php print $options['left01-color-hover-rgb']?>;
 	padding: 3px;
 	border: 1px solid <?php print $options['left01-border-top']?>;
 }
+
+.breadcrumbs a {
+	color: <?php print $options['linkcolor']?>;
+}
+
 
 /* Begin post/page entry css
 -------------------------------------------------------------- */						
