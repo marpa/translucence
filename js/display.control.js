bd8cd39b7a732ae5ce01b7db847b0b2f00fad237
Chapin <achapin@achapin-7660.local>
2010-12-10 10:10:20 -0500

    added javascript to get last toggle state from cookies and refined toggle css

--- js/display.control.js
+++ js/display.control.js
@@ -9,11 +9,33 @@ function changeText(el, newText) {
 	}
 }
 
+function setToggleFromCookie (primary_width, secondary_width, tertiary_width, content_width) {
+	var cookieprimary = document.cookie.indexOf("hideprimary=");
+	var cookiesecondary = document.cookie.indexOf("hidesecondary=");
+	var cookietertiary = document.cookie.indexOf("hidetertiary=");
+	
+	cookieprimary = getCookie("hideprimary");
+	cookiesecondary = getCookie("hidesecondary");
+	cookietertiary = getCookie("hidetertiary");
+	
+	if (cookieprimary > -1 && cookieprimary == 1) {
+		toggle('primary', primary_width, secondary_width, tertiary_width, content_width);
+	}
+	if (cookiesecondary > -1 && cookiesecondary == 1) {
+		toggle('secondary', primary_width, secondary_width, tertiary_width, content_width);
+	}
+	if (cookietertiary > -1 && cookietertiary == 1) {
+		toggle('tertiary', primary_width, secondary_width, tertiary_width, content_width);
+	}
+	
+}
+
 function toggle(obj, primary_width, secondary_width, tertiary_width, content_width) {
 	var box = document.getElementById(obj);	
 	var primarybox = document.getElementById('primary');
 	var secondarybox = document.getElementById('secondary');
 	var tertiarybox = document.getElementById('tertiary');
+	//alert(box);
 	
 	var widgetlist = box.getElementsByTagName('ul')[0];
 	var toggle_link_element = "toggle"+obj;
@@ -22,8 +44,7 @@ function toggle(obj, primary_width, secondary_width, tertiary_width, content_wid
 	var new_content_width;
 	var current_primary_width;
 	var current_secondary_width;
-	var current_tertiary_width;
-	
+	var current_tertiary_width;	
 
 	switch (obj) {
 	case "primary": 
@@ -36,8 +57,6 @@ function toggle(obj, primary_width, secondary_width, tertiary_width, content_wid
 		default_box_width = tertiary_width;
 		break;
 	}	
-
-	
 	
 	if (primarybox != null) current_primary_width = primarybox.style.width.replace("px", "");
 	if (secondarybox != null) current_secondary_width = secondarybox.style.width.replace("px", "");
@@ -49,22 +68,36 @@ function toggle(obj, primary_width, secondary_width, tertiary_width, content_wid
 	if (current_tertiary_width == 0) width_adjust = width_adjust + Number(tertiary_width);
 	
 	box_width = default_box_width+"px";
-	//alert("style: "+box.style.width+"  default: "+box_width);
-	//alert(box_width);
 	
 	if (box.style.width != box_width) {
 		box.style.width = box_width;
 		new_content_width = (Number(content_width) + Number(width_adjust) - Number(default_box_width))+"px";
 		document.getElementById('content').style.width = new_content_width;
 		widgetlist.style.display = 'block';
-		document.cookie = "hidebox=0";
+		document.cookie = "hide"+obj+"=0";
 		changeText(toggleLink, "-");
 	} else {
 		box.style.width = '0px';
 		new_content_width = (Number(content_width) + Number(width_adjust) + Number(default_box_width))+"px";
 		document.getElementById('content').style.width = new_content_width;
 		widgetlist.style.display = 'none';
-		document.cookie = "hidebox=1";
+		document.cookie = "hide"+obj+"=1";
 		changeText(toggleLink, "+");
 	}	
+}
+
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=");
+  if (c_start!=-1)
+    {
+    c_start=c_start + c_name.length+1;
+    c_end=document.cookie.indexOf(";",c_start);
+    if (c_end==-1) c_end=document.cookie.length;
+    return unescape(document.cookie.substring(c_start,c_end));
+    }
+  }
+return "";
 }
