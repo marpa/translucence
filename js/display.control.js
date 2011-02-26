d3e2498812c84bf23d0dc955ed89b470f35415bb
Alex Chapin <achapin@gmail.com>
2011-02-26 15:11:35 -0500

    added comments to toogle()

--- js/display.control.js
+++ js/display.control.js
@@ -41,6 +41,7 @@ function setToggleFromCookie (primary_width, secondary_width, tertiary_width, co
 	
 }
 
+// Toggle the visibility of the object, update content width to new context and update toggle links
 function toggle(obj, context, primary_width, secondary_width, tertiary_width, content_width) {
 	var box = document.getElementById(obj);	
 	var primarybox = document.getElementById('primary');
@@ -48,73 +49,93 @@ function toggle(obj, context, primary_width, secondary_width, tertiary_width, co
 	var tertiarybox = document.getElementById('tertiary');
 	//alert(box);
 	
+	// initialize variable for:
+	// all toggle links
+	// current sidebar box widths
+	// initial box width for box to be toogle
+	// new content width
 	if (box) {
-	var widgetlist = box.getElementsByTagName('ul')[0];
-	var toggle_link_element = "toggle"+obj;
-	var toggle_context_element = "togglecontent"+obj;
-	var toggleLink = document.getElementById(toggle_link_element);
-	var togglecontextlink = document.getElementById(toggle_context_element);
-	var default_box_width;
-	var new_content_width;
-	var current_primary_width;
-	var current_secondary_width;
-	var current_tertiary_width;	
-	
-
-	switch (obj) {
-	case "primary": 
-		default_box_width = primary_width;
-		break;
-	case "secondary": 
-		default_box_width = secondary_width;
-		break;
-	case "tertiary": 
-		default_box_width = tertiary_width;
-		break;
-	}	
-	
-	if (primarybox != null) current_primary_width = primarybox.style.width.replace("px", "");
-	if (secondarybox != null) current_secondary_width = secondarybox.style.width.replace("px", "");
-	if (tertiarybox != null) current_tertiary_width = tertiarybox.style.width.replace("px", "");
-	var width_adjust = 0;
-	
-	if (current_primary_width == 0) width_adjust = Number(primary_width);
-	if (current_secondary_width == 0) width_adjust = width_adjust + Number(secondary_width);
-	if (current_tertiary_width == 0) width_adjust = width_adjust + Number(tertiary_width);
-	
-	box_width = default_box_width+"px";
-	
-	if (box.style.width != box_width) {
-		box.style.width = box_width;
-		box.style.display = "block";
-		new_content_width = (Number(content_width) + Number(width_adjust) - Number(default_box_width))+"px";
-		document.getElementById('content').style.width = new_content_width;
-		widgetlist.style.display = 'block';
-		document.cookie = "hide"+obj+"=0";
-		if (context == "content") {	
-			if (obj == "tertiary") {
-				changeText(togglecontextlink, "»");
+		var widgetlist = box.getElementsByTagName('ul')[0];
+		var toggle_link_element = "toggle"+obj;
+		var toggle_context_element = "togglecontent"+obj;
+		var toggleLink = document.getElementById(toggle_link_element);
+		var togglecontextlink = document.getElementById(toggle_context_element);
+		var default_box_width;
+		var new_content_width;
+		var current_primary_width;
+		var current_secondary_width;
+		var current_tertiary_width;	
+		
+		// get width of box to be toggled
+		switch (obj) {
+		case "primary": 
+			default_box_width = primary_width;
+			break;
+		case "secondary": 
+			default_box_width = secondary_width;
+			break;
+		case "tertiary": 
+			default_box_width = tertiary_width;
+			break;
+		}	
+		
+		// get current widths of each box
+		if (primarybox != null) current_primary_width = primarybox.style.width.replace("px", "");
+		if (secondarybox != null) current_secondary_width = secondarybox.style.width.replace("px", "");
+		if (tertiarybox != null) current_tertiary_width = tertiarybox.style.width.replace("px", "");
+		var width_adjust = 0;
+		
+		if (current_primary_width == 0) width_adjust = Number(primary_width);
+		if (current_secondary_width == 0) width_adjust = width_adjust + Number(secondary_width);
+		if (current_tertiary_width == 0) width_adjust = width_adjust + Number(tertiary_width);
+		
+		// width of box to be toggled
+		box_width = default_box_width+"px";
+		
+		// if current box width is NOT its default width 
+		// update its width to default and set display to block
+		// calculate new content width
+		// change toggle link text to expand text
+		// set document cookie to hide = 0	
+		if (box.style.width != box_width) {
+			box.style.width = box_width;
+			box.style.display = "block";
+			new_content_width = (Number(content_width) + Number(width_adjust) - Number(default_box_width))+"px";
+			document.getElementById('content').style.width = new_content_width;
+			widgetlist.style.display = 'block';
+			document.cookie = "hide"+obj+"=0";
+			
+			// update toggle links based on context and location
+			if (context == "content") {	
+				if (obj == "tertiary") {
+					changeText(togglecontextlink, "»");
+				} else {
+					changeText(togglecontextlink, "«");
+				}
+			} else if (obj == "tertiary") {
+				changeText(toggleLink, "»");
 			} else {
-				changeText(togglecontextlink, "«");
+				changeText(toggleLink, "«");
 			}
-		} else if (obj == "tertiary") {
-			changeText(toggleLink, "»");
+			
+		// if current box IS its default width
+		// update its width to 0 and set display to none
+		// calculate new content width
+		// change toggle link text to collapse text
+		// set document cookie to hide = 1
 		} else {
-			changeText(toggleLink, "«");
-		}
-	} else {
-		box.style.width = '0px';
-		box.style.display = "none";
-		new_content_width = (Number(content_width) + Number(width_adjust) + Number(default_box_width))+"px";
-		document.getElementById('content').style.width = new_content_width;
-		widgetlist.style.display = 'none';
-		document.cookie = "hide"+obj+"=1";
-		if (obj == "tertiary") {
-			changeText(togglecontextlink, "«");
-		} else {
-			changeText(togglecontextlink, "»");
-		}
-	}	
+			box.style.width = "0px";
+			box.style.display = "none";
+			new_content_width = (Number(content_width) + Number(width_adjust) + Number(default_box_width))+"px";
+			document.getElementById('content').style.width = new_content_width;
+			widgetlist.style.display = 'none';
+			document.cookie = "hide"+obj+"=1";
+			if (obj == "tertiary") {
+				changeText(togglecontextlink, "«");
+			} else {
+				changeText(togglecontextlink, "»");
+			}
+		}	
 	}
 }
 
