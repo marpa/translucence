a665abf3727562754f98fae55e72a14f5659ffa5
Alex Chapin <achapin@gmail.com>
2011-02-26 14:11:01 -0500

    increased sidebar top margin

--- variations/default/variation.php
+++ variations/default/variation.php
@@ -103,10 +103,10 @@ $options['content-margin-right'] = "1";
 $options['right01-margin-right'] = "1";
 $options['right02-margin-right'] = "0";
 
-$options['left01-margin-top'] = "0";
+$options['left01-margin-top'] = "5";
 $options['content-margin-top'] = "0";
-$options['right01-margin-top'] = "0";
-$options['right02-margin-top'] = "0";
+$options['right01-margin-top'] = "5";
+$options['right02-margin-top'] = "5";
 
 
 /******************************************************************************
