dd872a44bcc015997759f0072aeff2a8855f4248
Alex Chapin <achapin@gmail.com>
2011-03-02 16:28:42 -0500

    updated add_menu_admin_bar() to return instead of exit if user is not a super admin or the admin bar is not showing

--- functions.php
+++ functions.php
@@ -84,7 +84,7 @@ function add_menu_admin_bar() {
     global $wp_admin_bar;
     
     if ( !is_super_admin() || !is_admin_bar_showing() )
-        exit; 
+        return; 
         
     $wp_admin_bar->add_menu( array( 'parent' => 'appearance', 'title' =>__( 'Design', 'design' ), 'href' => admin_url('admin.php')."/themes.php?page=Variations" ) );
 }
