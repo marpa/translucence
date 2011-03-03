fc9fe88e44b198ac8b7bd7befe6b4df40ca76503
Chapin <achapin@achapin-7660.local>
2011-03-03 16:39:34 -0500

    updated author template to include toggle sidebar links and to display pages by author title and list only if these exist

--- author.php
+++ author.php
@@ -19,6 +19,15 @@ $author = get_userdata($wp_the_query->query_vars['author']);
 		<?php get_sidebar('tertiary'); ?>
 		<?php endif; ?>	
 			<div id="content" role="main" style="width: <?php print $content_width; ?>px;">
+		<div class="toggle" style="float: left;">
+			<a id="togglecontenttertiary" href="javascript:toggle('tertiary','content',<?php print get_box_widths(); ?>)">&nbsp;</a>
+		</div>
+		<div class="toggle" style="float: right;">
+			<a id="togglecontentsecondary" href="javascript:toggle('secondary','content',<?php print get_box_widths(); ?>)">&nbsp;</a>
+		</div>
+		<div class="toggle">
+			<a id="togglecontentprimary" href="javascript:toggle('primary','content',<?php print get_box_widths(); ?>)">&nbsp;</a>
+		</div>
 
 <?php
 	/* Queue the first post, that way we know who
@@ -36,36 +45,35 @@ $author = get_userdata($wp_the_query->query_vars['author']);
 </div>
 <br/>
 
-					<div id="entry-author-info">
-						<div id="author-avatar">
-							<?php echo get_avatar( $author->user_email, apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
-						</div><!-- #author-avatar -->
-						<div id="author-description">							
-							<h2><?php print $author->display_name; ?></h2>
-							<div><a href="<?php print $author->user_url; ?>"><?php print $author->user_url; ?></a></div>
-							<div><?php print $author->description; ?></div>
-						</div><!-- #author-description	-->
-					</div><!-- #entry-author-info -->
+		<div id="entry-author-info">
+			<div id="author-avatar">
+				<?php echo get_avatar( $author->user_email, apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
+			</div><!-- #author-avatar -->
+			<div id="author-description">							
+				<h2><?php print $author->display_name; ?></h2>
+				<div><a href="<?php print $author->user_url; ?>"><?php print $author->user_url; ?></a></div>
+				<div><?php print $author->description; ?></div>
+			</div><!-- #author-description	-->
+		</div><!-- #entry-author-info -->
 <div id="entry-author-pages">
 <div id="toc" class="toc">
+<?php 
+$authorpages = wp_list_pages("title_li=&author=".$author->ID."&echo=0");
+$num_authorpages = get_pages("authors=".$author->ID);
+if ($authorpages && count($num_authorpages) > 0) { ?>
 <h3>Pages by <?php print $author->display_name; ?></h3>
 <div class="toggle">
-	<a id="togglelink" href="javascript:toggleToc()">-</a>
+	<a id="togglelink" href="javascript:toggleToc()">[show page links]</a>
 </div>
-
+<?php } ?>
 <?php
-	$authorpages = wp_list_pages("title_li=&author=".$author->ID."&echo=0");
-	$num_authorpages = get_pages("authors=".$author->ID);
-
-	print "<div class='pagenav' style='margin-bottom: 15px;'>";
-	print "<ul>";	
 	if ($authorpages && count($num_authorpages) > 0 && $author->ID != 0) {
+		print "<div class='pagenav' style='margin-bottom: 15px;'>";
+		print "<ul>";	
 		print $authorpages;
-	} else {
-		print $author->display_name." has not created any pages on this site...";
+		print "</ul>";
+		print "</div>";
 	}
-	print "</ul>";
-	print "</div>";
 ?>
 </div>
 
