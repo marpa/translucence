0fbd7e00410c0f450b8f915a3dbac077b142499d
Chapin <achapin@achapin-7660.local>
2011-03-13 11:18:07 -0400

    updated default variation to set headermeta to off

--- variations/default/variation.php
+++ variations/default/variation.php
@@ -155,7 +155,7 @@ $options['searchbox-color'] = "#FFFFFF";
  * Set these only if not in the initial options array
  ******************************************************************************/
 
-if (!isset($options['headermeta'])) $options['headermeta'] = "on";
+if (!isset($options['headermeta'])) $options['headermeta'] = "off";
 if (!isset($options['options-mode'])) $options['options-mode'] = "basic";
 
 if (!isset($options['background'])) $options['background'] = "translucence-gray-white";
