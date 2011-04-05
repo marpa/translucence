7200fce4b869b1a99a4576a021c6967e4c5fc2bd
Alex Chapin <achapin@gmail.com>
2011-04-05 17:55:17 -0400

    added translucence_toggle_links() to tag template

--- tag.php
+++ tag.php
@@ -18,7 +18,7 @@ $content_width = translucence_get_content_width ("tag");
 	<?php get_sidebar('tertiary'); ?>
 	<?php endif; ?>	
 			<div id="content" role="main" style="width: <?php print $content_width; ?>px;">
-<?php print translucence_toggle_links(); ?>			
+		<?php print translucence_toggle_links(); ?>			
 			<div style = "width: 50%; float: right;">
 				<div id="syndication">
 				<a href="<?php print get_tag_feed_link( $tag_id, 'rss2' ); ?>" class="feed">
