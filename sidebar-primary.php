daeda30774b1cb51fec5b771d99a5479be2549f9
Chapin <achapin@achapin-7660.local>
2011-04-27 17:54:16 -0400

    added translucence_get_default_widgets() to main sidebars to allow for setting of default widgets based on config

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The Sidebar containing the primary and secondary widget areas.
+ * The Sidebar containing the primary widget areas.
  *
  * @package WordPress
  * @subpackage 2010_Translucence
@@ -14,41 +14,23 @@
 	 * then the sidebar simply doesn't exist, so we'll hard-code in
 	 * some default sidebar stuff just in case.
 	 */
-	global $translucence_options;
+	global $translucence_options; ?>
 	
-	if ( $translucence_options['right01-width'] != '0') : ?>
+	<div id="primary" class="widget-area" role="complementary" style="<?php translucence_box_display('primary') ?>">
+		<div class="toggle" style="margin-right: -20px;">
+		<a id="toggleprimary" href="javascript:toggle('primary','sidebar',<?php print translucence_get_box_widths(); ?>)">&laquo;</a>
+		</div>
+		<ul class="xoxo">
+			<?php 				
+			if ( !is_active_sidebar( 'primary-widget-area' )) {
+				translucence_get_default_widgets('primary-widget-area');
+								
+			} else {
+				dynamic_sidebar( 'primary-widget-area' ); 		
+			}
+			?>
+		</ul>
+	</div><!-- #primary .widget-area -->
 	
-		<div id="primary" class="widget-area" role="complementary" style="<?php translucence_box_display('primary') ?>">
-			<div class="toggle" style="margin-right: -20px;">
-			<a id="toggleprimary" href="javascript:toggle('primary','sidebar',<?php print translucence_get_box_widths(); ?>)">&laquo;</a>
-			</div>
-			<ul class="xoxo">
-				<?php dynamic_sidebar( 'primary-widget-area' ); ?>
-			</ul>
-		</div><!-- #primary .widget-area -->
 
-	<?php else : ?>
-	
-	
-			<li id="search" class="widget-container widget_search">
-				<?php get_search_form(); ?>
-			</li>
-
-			<li id="archives" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Archives', '2010-translucence' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
-
-			<li id="meta" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Meta', '2010-translucence' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
-
-	<?php endif; // end primary widget area ?>
 
