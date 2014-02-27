655345cc851e545e29a8661c9503c808b36dfd95
Alex Chapin <achapin@gmail.com>
2014-02-27 15:14:23 -0500

    comment out viewport meta-tag

--- header.php
+++ header.php
@@ -43,7 +43,9 @@ session_start();
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 	<?php if( $mobile_support->is_mobile ): ?>
+<!-- 
 		<meta name="viewport" content="user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi">
+ -->
 	<?php endif; ?>
 	
 
